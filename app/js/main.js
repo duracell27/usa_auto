2c79cebca369ea93d3fa8593a036af6eea3c2750
duracell27 <volodymyrshmidt@gmail.com>
2021-04-11 19:16:38 +0300

    USA AUTO READY

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,20 @@ $(function () {
         dots: true,
         arrows: false,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings:{
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 601,
+                settings:{
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 })
